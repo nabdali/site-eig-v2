b78123b72b23c52599bf8bd5efc8f1d33109ec36
elisabethfainstein <elisabeth.fainstein@data.gouv.fr>
2022-08-02 17:31:12 +0200

    (refactor) remove title from shortcode definition

--- src/js/shortcodes/dailymotion.js
+++ src/js/shortcodes/dailymotion.js
@@ -2,25 +2,20 @@ export default {
     id: "dailymotion",
     label: "Dailymotion",
     fields: [{
-        name: "videoID",
+        name: "shortcode",
         label: "Dailymotion Video ID",
         widget: "string"
-    }, {
-        name: "videoTitle",
-        label: "Dailymotion Video Title",
-        widget: "string"
     }],
-    pattern: /{{< dailymotion ([a-zA-Z0-9]+) ([a-zA-Z0-9]+)>}}/,
+    pattern: /{{< dailymotion ([a-zA-Z0-9]+) >}}/,
     fromBlock: function (match) {
         return {
-            videoID: match[1],
-            videoTitle: match[2]
+            shortcode: match[1]
         };
     },
     toBlock: function (obj) {
-        return `{{< dailymotion ${obj.videoID} ${obj.videoTitle} >}}`;
+        return `{{< dailymotion ${obj.shortcode} >}}`;
     },
     toPreview: function (obj) {
-        return `<div class="c-embed fr-mb-8v"><iframe frameborder="0" type="text/html" src="https://www.dailymotion.com/embed/video/${obj.videoID}" title=${obj.videoTitle} width="100%" height="100%" allowfullscreen></iframe></div>`;
+        return `<div class="c-embed fr-mb-8v"><iframe frameborder="0" type="text/html" src="https://www.dailymotion.com/embed/video/${obj.shortcode}" width="100%" height="100%" allowfullscreen></iframe></div>`;
     },
 }
